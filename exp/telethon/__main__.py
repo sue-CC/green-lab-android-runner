6c6c08e70a669ef89f38ce768fcd9aaae66d18c4
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2023-10-08 23:04:10 +0200

    update py script

--- exp/telethon/__main__.py
+++ exp/telethon/__main__.py
@@ -3,6 +3,7 @@ import os.path
 import random
 import time
 from telethon import TelegramClient, sync
+import argparse
 
 # These example values won't work. You must get your own api_id and
 # api_hash from https://my.telegram.org, under API Development.
@@ -13,20 +14,33 @@ from telethon import TelegramClient, sync
 # joeyyyy12
 
 with open(os.path.dirname(os.path.abspath(__file__)) + '/config.json', 'r') as json_file:
-        data = json.load(json_file)
-        api_id = data["api_id"]
-        api_hash = data["api_hash"]
-        session_name = data["session_name"]
-        receiver = data["receiver"]
-        messages = data["messages"]
-        nr_of_messages = data["nr_of_messages"]
-        sending_window = data["sending_window"]
-        setup_time = data["setup_time"]
-        receiving_buffer = data["receiving_buffer"]
-        burst = data["burst"]
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--nr_of_messages', action='store')
+    parser.add_argument('--burst', action='store_true')
+    arg: argparse.Namespace = parser.parse_args()
+    data = json.load(json_file)
+    api_id = data["api_id"]
+    api_hash = data["api_hash"]
+    session_name = data["session_name"]
+    receiver = data["receiver"]
+    messages = data["messages"]
+    nr_of_messages = data["nr_of_messages"]
+    sending_window = data["sending_window"]
+    setup_time = data["setup_time"]
+    receiving_buffer = data["receiving_buffer"]
+    burst = data["burst"]
+    # command-line arguments override the corresponding entries in config.json
+    try:
+        nr_of_messages = arg.nr_of_messages
+    except AttributeError as e:
+        pass
+    try:
+        burst = arg.burst
+    except AttributeError as e:
+        pass
 
 client = TelegramClient(session_name, api_id, api_hash).start()
+
 
 # def connect():
 #     me = client.get_me()
@@ -36,16 +50,21 @@ def send_message():
     message = random.choice(messages)
     message = "[" + time.strftime(" at %H:%M:%S", time.localtime()) + "] " + message
     client.send_message(receiver, message)
-    print( message )
+    print(message)
+
 
 def auto_sending():
-    if burst:
-        send_interval = 0.5
-    else: 
-        send_interval = sending_window / nr_of_messages
-    for _ in range(nr_of_messages):
-        send_message()
-        time.sleep(send_interval)
+    if nr_of_messages == 0:
+        time.sleep(sending_window)
+    else:
+        if burst:
+            send_interval = 0.2
+        else:
+            send_interval = sending_window / nr_of_messages
+        for _ in range(nr_of_messages):
+            send_message()
+            time.sleep(send_interval)
+
 
 def repeat_auto_sending():
     time.sleep(setup_time)
@@ -54,9 +73,11 @@ def repeat_auto_sending():
     auto_sending()
     # time.sleep(receiving_buffer)
 
+
 def main():
     print("sending messages to ====> " + receiver)
     repeat_auto_sending()
+
 
 if __name__ == "__main__":
     main()
