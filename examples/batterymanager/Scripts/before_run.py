51b934aa69dea638d9f9c75ea727c4e2e40d85b4
Yuna C <yunabell@outlook.com>
2023-10-09 17:09:07 +0200

    initial update

--- examples/batterymanager/Scripts/before_run.py
+++
@@ -1,8 +0,0 @@
-import time
-
-
-# noinspection PyUnusedLocal
-def main(device, *args, **kwargs):
-    device.shell('am start -n "com.example.batterymanager_utility/com.example.batterymanager_utility.MainActivity" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER')
-    time.sleep(5)
-
