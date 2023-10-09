cad06c995d4629f7291d78c7dbd8fd835886ce79
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2023-10-09 12:40:53 +0200

    Update before_run.py

--- examples/batterymanager/Scripts/before_run.py
+++ examples/batterymanager/Scripts/before_run.py
@@ -3,7 +3,7 @@ import time
 
 # noinspection PyUnusedLocal
 def main(device, *args, **kwargs):
+    device.shell('input keyevent 26') # lock screen
     device.shell('am start -n "com.example.batterymanager_utility/com.example.batterymanager_utility.MainActivity" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER')
-    device.shell('input keyevent 26'); # lock screen
     # time.sleep(5)
 
