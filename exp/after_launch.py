fd4be772ed33854f03f23ceab738b1c36bf896cd
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2023-10-10 13:17:55 +0200

    /

--- exp/after_launch.py
+++ exp/after_launch.py
@@ -1,2 +1,8 @@
+import time
+
+
 def main(device, *args, **kwargs):
+    time.sleep(5)  # wait for the completion of page load (mainly for Firefox)
+    device.shell('input keyevent 26')  # lock screen
+    time.sleep(1)
     pass
--- exp/before_run.py
+++ exp/before_run.py
@@ -1,3 +1,2 @@
 def main(device, *args, **kwargs):
-    device.shell('input keyevent 26')  # lock screen
     pass
